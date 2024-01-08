924cedfb9b7363a2326b082ddc4160d2f2226241
pxthaphi <ohmzaza21@gmail.com>
2024-01-08 10:56:28 +0700

    อัพแล้วนะจ๊ะ

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:lab07_126/pages/firstpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dropdown List',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const FirstPage(),
+    );
+  }
+}
